a0ff34d1a2ac907d3eb872cf2eba81da695cfc28
Ritesh Verma <riteshverma312@gmail.com>
2026-02-22 12:42:11 -0500

    fix: Crop SVG viewBox to logo artwork bounds for proper display size

--- components/Navbar.js
+++ components/Navbar.js
@@ -72,6 +72,7 @@ export default function Navbar() {
                         src="/logo.svg"
                         alt="Verma's Furnishing"
                         className={styles.logoImage}
+                        style={{ height: '65px', width: 'auto', display: 'block' }}
                     />
                 </Link>
 
